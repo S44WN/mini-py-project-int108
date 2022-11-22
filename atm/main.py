4a1ddc9d9303dbf86f10224dde2a025ce7e4c4a6
Sawan <itissaavn@gmail.com>
2022-11-22 12:30:37 +0530

    add atm ascii art

--- atm/main.py
+++ atm/main.py
@@ -4,3 +4,16 @@ user_balance = 97432.50
 user_name = "Mr John Doe"
 user_pin = 1234
 user_verification = False
+
+print("""
+\n
+\t\t\t         _             
+\t\t\t        | |            
+\t\t\t  __ _  | |_   _ __ ___  
+\t\t\t / _` | | __| | '_ ` _ \ 
+\t\t\t| (_| | | |_  | | | | | |
+\t\t\t \__,_|  \__| |_| |_| |_|
+\n
+""")
+
+print("Welcome to your bank account", user_name, end="\n\n")
